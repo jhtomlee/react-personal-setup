ec5271d2d83b98c6ece832c5d95ab659186dadc2
jhtomlee <jhtomlee@gmail.com>
2020-08-19 12:27:07 -0400

    Set up Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Typography, Box, Link } from '@material-ui/core';
+import ProTip from './components/ProTip';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        jhtomlee
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          My React Set-up
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
